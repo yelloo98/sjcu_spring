b9f66bf55262e550be8cc6e6ce0dc6097828abe8
yelloo98 <yelloo98@naver.com>
2024-12-08 20:49:02 +0900

    add test

--- src/test/java/org/zerock/board/repository/BoardRepositoryTests.java
+++ src/test/java/org/zerock/board/repository/BoardRepositoryTests.java
@@ -4,22 +4,19 @@ package org.zerock.board.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.transaction.annotation.Transactional;
 import org.zerock.board.entity.Board;
 import org.zerock.board.entity.Member;
+import org.zerock.board.service.BoardService;
 
-import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 @SpringBootTest
-@EnableJpaAuditing
+//@EnableJpaAuditing
 public class BoardRepositoryTests {
 
     @Autowired
@@ -38,7 +35,7 @@ public class BoardRepositoryTests {
     @Test
     public void testRead1() {
     	//조인명령을 활용한다.
-        Optional<Board> result = boardRepository.findById(101L); //데이터베이스에 존재하는 번호
+        Optional<Board> result = boardRepository.findById(105L); //데이터베이스에 존재하는 번호
 
         Board board = result.get();
         
@@ -48,7 +45,39 @@ public class BoardRepositoryTests {
 
     }
 
+    @Test
+    public void getListTest() {
+        System.out.println("getList()..............Start");
+        List<Board> boardList = boardRepository.findAll()
+                .stream()
+                .sorted(Comparator.reverseOrder())
+                .limit(10)
+                .collect(Collectors.toList());
 
+        System.out.println(boardList);
+        System.out.println("getList()..............End");
+    }
+
+    @Test
+    public void modifyTest() {
+        System.out.println("modifyTest()..............Start");
+        Member member = Member.builder().email("user100@aaa.com").build();
+        Board board = Board.builder()
+                .bno(205L)
+                .title("modifyTest...")
+                .content("modifyTestContent....")
+                .writer(member)
+                .build();
+        boardRepository.save(board);
+        System.out.println("modifyTest()..............End");
+    }
+
+    @Test
+    public void removeTest() {
+        System.out.println("removeTest()..............Start");
+        boardRepository.deleteById(205L);
+        System.out.println("removeTest()..............End");
+    }
 
     
 }
